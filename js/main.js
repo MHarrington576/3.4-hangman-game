7b45fe9f34555375dc0a5c5ed12bbd8f0ea09c34
Matthew Harrington <mattharrington576@gmail.com>
2016-10-15 19:50:39 -0400

    Attempted bug fixes

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@
   });
 
   var secretWord = randomization();
-  var livesLeft = 5;
+  var guessesLeft = 10;
   var guessButton = document.getElementById("guess-button");
     guessButton.addEventListener('click', retrieve());
   var wordContainer = document.querySelector(".unknown-word-container");
   var header = document.querySelector("h2");
   var correctGuesses = 0;
-  var remainingLives = document.getElementById("remaining-lives");
+  var remainingGuesses = document.getElementById("remaining-lives");
   var secretWordContainer = document.querySelector(".unknown-word-container");
 
   console.log(secretWord);
@@ -28,11 +28,12 @@
     var guess = document.getElementById("guess");
     var guessedLetter = guess.value;
     comparison(guessedLetter);
+    decreaseGuesses();
     guess.value = "";
   }
 
   function comparison(guessedLetter){
-    for (var i=0; i < secretWord.length; i++){
+    for (var i = 0; i < secretWord.length; i++){
       if(secretWord[i] == guessedLetter) {
         wordContainer.children[i].textContent = guessedLetter;
         correctGuesses += 1;
@@ -40,23 +41,23 @@
       if(correctGuesses == secretWord.length){
         header.textContent = "CONGRATULATIONS! YOU WON!"
       }
-      else{
-        decreaseLives();
-      }
     }
   }
 
-  function decreaseLives(){
-    for (var l=5; l < 1; l--){
-      remainingLives = l
-      return remainingLives;
-    }
-    if(remainingLives == 0){
+  function decreaseGuesses(){
+    remainingGuesses -= 1;
+    var guessNumber = document.getElementById("remaining-lives");
+    guessNumber.textContent = remainingGuesses;
+    // for (var l=5; l < 1; l--){
+    //   remainingGuesses = l
+    //   return remainingGuesses;
+    // }
+    if(remainingGuesses == 0){
       header.textContent = "Sorry, li'l sport. You'll get 'em next time."
     }
   }
 
-  for(var j=0; j < secretWord.length; j++){
+  for(var i = 0; i < secretWord.length; i++){
     var blankSpaces = document.createElement("span");
     var blankSpacesContent = document.createTextNode("_");
     secretWordContainer.appendChild(blankSpacesContent);
